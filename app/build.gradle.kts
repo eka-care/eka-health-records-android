0bc93fd43675594959730c524d0b49ef64fc7df0
ayushisingh <ayushisingh06192000@gmil.com>
2025-01-31 14:12:44 +0530

    reverted the version change

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -68,7 +68,7 @@ afterEvaluate {
 
                 groupId = "com.eka.records"
                 artifactId = "eka-records"
-                version = "3.1.2"
+                version = "3.1.1"
             }
         }
     }
