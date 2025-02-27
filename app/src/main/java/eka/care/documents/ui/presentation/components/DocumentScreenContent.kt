9598a2add3a9ff3f9e8399265c36f666b4b4372d
Ayushi20-alt <ayushisingh06192000@gmail.com>
2025-02-27 15:49:37 +0530

    pr review -3

--- app/src/main/java/eka/care/documents/ui/presentation/components/DocumentScreenContent.kt
+++ app/src/main/java/eka/care/documents/ui/presentation/components/DocumentScreenContent.kt
@@ -1,6 +1,5 @@
 package eka.care.documents.ui.presentation.components
 
-import android.util.Log
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
