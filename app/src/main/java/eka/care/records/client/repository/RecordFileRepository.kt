1965900d58a1f2313348a4a4e73ec513b9c06498
Sumit Kumar <77614924+skum-14@users.noreply.github.com>
2025-04-04 11:55:11 +0530

    Refactoring

--- app/src/main/java/eka/care/records/client/repository/RecordFileRepository.kt
+++
@@ -1,8 +0,0 @@
-package eka.care.records.client.repository
-
-import eka.care.records.data.entity.RecordFile
-
-interface RecordFileRepository {
-    suspend fun insertRecordFile(file: RecordFile): Long
-    suspend fun getRecordFile(localId: String): List<RecordFile>?
-}
