09edba4a82d0d95a636e3fd27f5d49e5aeb68416
Sumit Kumar <77614924+skum-14@users.noreply.github.com>
2025-04-07 18:23:01 +0530

    Edit and document count

--- app/src/main/java/eka/care/records/client/utils/RecordsUtility.kt
+++ app/src/main/java/eka/care/records/client/utils/RecordsUtility.kt
@@ -2,79 +2,16 @@ package eka.care.records.client.utils
 
 import android.content.Context
 import android.content.ContextWrapper
-import android.graphics.Bitmap
-import android.graphics.ImageDecoder
-import android.net.Uri
-import android.os.Build
-import android.provider.MediaStore
-import android.util.Log
 import android.webkit.MimeTypeMap
 import eka.care.records.data.repository.MyFileRepository
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import okhttp3.ResponseBody
 import java.io.File
-import java.io.IOException
-import java.text.SimpleDateFormat
-import java.util.Date
-import java.util.Locale
 import java.util.UUID
 
 class RecordsUtility {
     companion object{
-        fun loadFromUri(context: Context, photoUri: Uri): Bitmap? {
-            var bitmap: Bitmap? = null
-            try {
-                bitmap = if (Build.VERSION.SDK_INT > Build.VERSION_CODES.O_MR1) {
-                    val source: ImageDecoder.Source =
-                        ImageDecoder.createSource(context.contentResolver, photoUri)
-                    ImageDecoder.decodeBitmap(source)
-                } else {
-                    @Suppress("DEPRECATION")
-                    MediaStore.Images.Media.getBitmap(context.contentResolver, photoUri)
-                }
-            } catch (ex: IOException) {
-                Log.e("log", "Exception in loadFromUri() = ", ex)
-            }
-            return bitmap
-        }
-        fun convertLongToFormattedDate(timestamp: Long?): String? {
-            if (timestamp == null) {
-                return null
-            }
-            val date = Date(timestamp * 1000)
-            val outputFormat = SimpleDateFormat("dd-MM-yyyy", Locale.getDefault())
-            return outputFormat.format(date)
-        }
-
-        fun timestampToLong(timestamp: String, format: String = "EEE, dd MMM, yyyy"): Long? {
-            if (timestamp == "Add Date") {
-                return null
-            }
-            val dateFormat = SimpleDateFormat(format, Locale.getDefault())
-            val date = dateFormat.parse(timestamp) ?: throw IllegalArgumentException("Invalid date format")
-            return date.time / 1000
-        }
-
-        fun formatLocalDateToCustomFormat(date: Date): String? {
-            val formatter = SimpleDateFormat("EEE, dd MMM, yyyy", Locale.getDefault())
-            return formatter.format(date)
-        }
-
-        fun changeDateFormat(inputDate: String?): String {
-            val inputFormat = SimpleDateFormat("yyyy-MM-dd", Locale.getDefault())
-            val outputFormat = SimpleDateFormat("dd-MM-yyyy", Locale.getDefault())
-            val date = inputFormat.parse(inputDate)
-            return date?.let { outputFormat.format(it) } ?: ""
-        }
-
-        fun convertLongToDateString(time: Long?): String {
-            if (time == null) return ""
-            val date = Date(time * 1000)
-            val format = SimpleDateFormat("dd EEE yyyy", Locale.getDefault())
-            return format.format(date)
-        }
-
         suspend fun downloadFile(url: String?, context: Context, type: String?): String {
             if (url == null) return ""
 
--- app/src/main/java/eka/care/records/client/utils/ThumbnailGenerator.kt
+++ app/src/main/java/eka/care/records/client/utils/ThumbnailGenerator.kt
@@ -6,7 +6,6 @@ import android.graphics.Canvas
 import android.graphics.Color
 import android.graphics.pdf.PdfRenderer
 import android.os.ParcelFileDescriptor
-import android.util.Log
 import java.io.File
 import java.io.FileOutputStream
 
@@ -14,53 +13,30 @@ class ThumbnailGenerator {
     companion object {
         fun getThumbnailFromPdf(app: Application, pdfFile: File): String? {
             try {
-                // Reading pdf in READ Only mode.
                 val fileDescriptor =
                     ParcelFileDescriptor.open(pdfFile, ParcelFileDescriptor.MODE_READ_ONLY)
-
-                // Initializing PDFRenderer object.
                 val renderer = PdfRenderer(fileDescriptor)
-
-                // Getting Page object by opening page.
                 val page = renderer.openPage(0)
-
-                // Creating empty bitmap. Bitmap.Config can be changed.
                 val bitmap =
                     Bitmap.createBitmap(page.width, page.height, Bitmap.Config.ARGB_8888)
-
-                // Creating Canvas from bitmap.
                 val canvas = Canvas(bitmap)
-
-                // Set White background color.
                 canvas.drawColor(Color.WHITE)
-
-                // Draw bitmap.
                 canvas.drawBitmap(bitmap, 0f, 0f, null)
-
-                // Render bitmap and can change mode too.
                 page.render(bitmap, null, null, PdfRenderer.Page.RENDER_MODE_FOR_DISPLAY)
-
-                // closing page
                 page.close()
-
-                // saving image into sdcard.
                 val file = File(app.cacheDir, "image${System.currentTimeMillis()}.png")
-
-                // check if file already exists, then delete it.
                 if (file.exists()) file.delete()
-
-                // Saving image in PNG format with 100% quality.
                 try {
                     val out = FileOutputStream(file)
                     bitmap.compress(Bitmap.CompressFormat.PNG, 100, out)
-                    Log.d("log", "Saved Image - ${file.absolutePath}")
                     out.flush()
                     out.close()
                 } catch (e: Exception) {
-                    Log.e("log", "Exception in saving file = ", e)
+                    Logger.e("Error saving thumbnail: ${e.message}")
                 }
                 return file.path
             } catch (ex: Exception) {
+                Logger.e("Error generating thumbnail: ${ex.message}")
                 return null
             }
         }
