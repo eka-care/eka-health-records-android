7dbc5f100e49e837f598caf523769c98a2d11cd0
Sumit Kumar <77614924+skum-14@users.noreply.github.com>
2025-05-12 19:42:00 +0530

    Logger

--- app/src/main/java/eka/care/records/client/model/EventLog.kt
+++ app/src/main/java/eka/care/records/client/model/EventLog.kt
@@ -1,7 +1,9 @@
 package eka.care.records.client.model
 
+import androidx.annotation.Keep
 import org.json.JSONObject
 
+@Keep
 data class EventLog(
     val params: JSONObject = JSONObject(),
     val message: String? = null,
