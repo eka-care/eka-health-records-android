fe1d63bcc38e73069146005d5f50b686972d11f4
Sumit Kumar <77614924+skum-14@users.noreply.github.com>
2025-05-13 10:38:12 +0530

    Keep annotation

--- app/src/main/java/eka/care/records/client/model/DocumentTypeCount.kt
+++ app/src/main/java/eka/care/records/client/model/DocumentTypeCount.kt
@@ -1,5 +1,8 @@
 package eka.care.records.client.model
 
+import androidx.annotation.Keep
+
+@Keep
 data class DocumentTypeCount(
     val documentType: String?,
     val count: Int?
--- app/src/main/java/eka/care/records/client/utils/DocumentConfiguration.kt
+++ app/src/main/java/eka/care/records/client/utils/DocumentConfiguration.kt
@@ -1,7 +1,9 @@
 package eka.care.records.client.utils
 
+import androidx.annotation.Keep
 import com.eka.network.IOkHttpSetup
 
+@Keep
 data class DocumentConfiguration(
     val okHttpSetup: IOkHttpSetup,
     val host: String,
